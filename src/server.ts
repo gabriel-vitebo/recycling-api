7f3e8de06d15c0bd765cec52ea9d051aa90cdb9b
gabriel-vitebo <vitebo@outlook.com>
2024-05-06 17:05:18 -0300

    :wrench: uploads: create route to uploads files

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import { env } from "./env";
 import { app } from "./app";
+import path from "node:path";
+
+
+app.register(require('@fastify/static'), {
+  root: path.resolve(__dirname, '..', 'uploads'),
+  prefix: '/uploads/'
+})
 
 app.listen({
   host: '0.0.0.0',
