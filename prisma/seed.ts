04cc8b5013a489218fec39beec0a99e37b71829c
gabriel-vitebo <vitebo@outlook.com>
2024-05-06 14:24:15 -0300

    :card_file_box: (database): add seed to create items in DB

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+interface DataSchema {
+  title: string,
+  image: string
+}
+
+async function main() {
+  const data: DataSchema[] = [
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+  ];
+
+  for (const item of data) {
+    await prisma.item.create({
+      data: item,
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
