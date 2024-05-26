3cd692bec7c91aff2706c7860cf2cb7770ed42c2
gabriel-vitebo <vitebo@outlook.com>
2024-05-26 16:54:53 -0300

    add more env variables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
@@ -10,12 +11,12 @@ interface DataSchema {
 
 async function main() {
   const data: DataSchema[] = [
-    { title: 'Lâmpadas', image: 'lampadas.svg', image_url: `http://localhost:3333/uploads/lampadas.svg` },
-    { title: 'Pilhas e Baterias', image: 'baterias.svg', image_url: `http://localhost:3333/uploads/baterias.svg` },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg', image_url: `http://localhost:3333/uploads/papeis-papelao.svg` },
-    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg', image_url: `http://localhost:3333/uploads/eletronicos.svg` },
-    { title: 'Resíduos Orgânicos', image: 'organicos.svg', image_url: `http://localhost:3333/uploads/organicos.svg` },
-    { title: 'Óleo de Cozinha', image: 'oleo.svg', image_url: `http://localhost:3333/uploads/oleo.svg` },
+    { title: 'Lâmpadas', image: 'lampadas.svg', image_url: `${env.UPLOADS_BASE_URL}/lampadas.svg` },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg', image_url: `${env.UPLOADS_BASE_URL}/baterias.svg` },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg', image_url: `${env.UPLOADS_BASE_URL}/papeis-papelao.svg` },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg', image_url: `${env.UPLOADS_BASE_URL}/eletronicos.svg` },
+    { title: 'Resíduos Orgânicos', image: 'organicos.svg', image_url: `${env.UPLOADS_BASE_URL}/organicos.svg` },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg', image_url: `${env.UPLOADS_BASE_URL}/oleo.svg` },
   ];
 
   for (const item of data) {
